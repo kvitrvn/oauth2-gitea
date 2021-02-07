7b7d3478214d4cc12e2d821ac5609437ad6f1812
Benjamin Gaudé <bgaude@cadoles.com>
2021-02-07 16:10:29 +0100

    Add some tests

--- test/src/Provider/GiteaTest.php
+++ test/src/Provider/GiteaTest.php
@@ -7,11 +7,13 @@ use FoxDeveloper\OAuth2\Client\Provider\Gitea;
 use League\OAuth2\Client\Tool\QueryBuilderTrait;
 use PHPUnit\Framework\TestCase;
 use Mockery as m;
+use Mockery\MockInterface;
 
 class GiteaTest extends TestCase
 {
 	use QueryBuilderTrait;
 
+	/** @var Gitea */
 	protected $provider;
 
 	protected function setUp(): void
@@ -20,6 +22,7 @@ class GiteaTest extends TestCase
 			'clientId' => 'mock_client_id',
 			'clientSecret' => 'mock_client_secret',
 			'redirectUri' => 'none',
+			'baseUrl' => 'https://gitea.example.com',
 		]);
 	}
 
@@ -32,7 +35,63 @@ class GiteaTest extends TestCase
 
 	public function testAuthorizationUrl()
 	{
-
-        $this->assertTrue(false);
+		$url = $this->provider->getAuthorizationUrl();
+        $uri = parse_url($url);
+		parse_str($uri['query'], $query);
+		
+		$this->assertArrayHasKey('client_id', $query);
+        $this->assertArrayHasKey('redirect_uri', $query);
+        $this->assertArrayHasKey('state', $query);
+        $this->assertArrayHasKey('scope', $query);
+        $this->assertArrayHasKey('response_type', $query);
+        $this->assertArrayHasKey('approval_prompt', $query);
+        $this->assertNotNull($this->provider->getState());
 	}
+
+	public function testScopes()
+    {
+        $options = ['scope' => [uniqid(), uniqid()]];
+
+		$url = $this->provider->getAuthorizationUrl($options);
+		
+		$this->assertStringContainsString(rawurlencode(implode(',', $options['scope'])), $url);
+    }
+
+    public function testGetAuthorizationUrl()
+    {
+        $url = $this->provider->getAuthorizationUrl();
+        $uri = parse_url($url);
+
+        $this->assertEquals('/login/oauth/authorize', $uri['path']);
+    }
+
+    public function testGetBaseAccessTokenUrl()
+    {
+        $params = [];
+
+        $url = $this->provider->getBaseAccessTokenUrl($params);
+        $uri = parse_url($url);
+
+        $this->assertEquals('/login/oauth/access_token', $uri['path']);
+    }
+
+    public function testGetAccessToken()
+    {
+		/** @var MockInterface */
+        $response = m::mock('Psr\Http\Message\ResponseInterface');
+        $response->shouldReceive('getBody')->andReturn('{"access_token":"mock_access_token", "scope":"repo,gist", "token_type":"bearer"}');
+        $response->shouldReceive('getHeader')->andReturn(['content-type' => 'json']);
+        $response->shouldReceive('getStatusCode')->andReturn(200);
+
+		/** @var MockInterface */
+        $client = m::mock('GuzzleHttp\ClientInterface');
+        $client->shouldReceive('send')->times(1)->andReturn($response);
+        $this->provider->setHttpClient($client);
+
+        $token = $this->provider->getAccessToken('authorization_code', ['code' => 'mock_authorization_code']);
+
+        $this->assertEquals('mock_access_token', $token->getToken());
+        $this->assertNull($token->getExpires());
+        $this->assertNull($token->getRefreshToken());
+    }
 }
